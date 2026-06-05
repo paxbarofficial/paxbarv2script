82fc3264569e36c5718ec992403a8397b618864e
paxbarofficial <paxbarcommunity@gmail.com>
2026-06-05 15:03:04 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+/* 
+- new release paxbar official version 2 I am gradually you are satisfied with the script that I developed thank you. 
+- if you like our script then Subscribe to our channel below, thank you again.
+
+channel_telegram : @paxbarvip
+channel_whatsapp : https://whatsapp.com/channel/0029VbBHYzSFsn0khhOvUj1m
+channel_youtube : https://youtube.com/@paxbarnewera
+
+NOTE :
+- This settings.js will be changed automatically when you run the paxbar version 2 script.
+- I warn you never to sell this script because this script is 100% free from me, and those who still insist on selling it will face the consequences.
+
+COPYRIGHT || PAXBAR OFFICIAL - 2026-2027
+*/
